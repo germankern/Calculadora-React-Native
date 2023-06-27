249cd0d133e47b26d2db679a59fad33f06e3bebe
germankern <germankern890@gmail.com>
2023-06-27 14:11:26 -0300

    Upload proyect in GitHub

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,44 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  fondo: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  calculadoraContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
+  },
+  resultadoPequeno: {
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  resultado: {
+    color: 'white',
+    fontSize: 60,
+    textAlign: 'right',
+    flexShrink: 1,
+    marginBottom: 10,
+  },
+  fila: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 18,
+    paddingHorizontal: 10,
+  },
+  boton: {
+    height: 80,
+    borderRadius: 100,
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  botonTexto: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 500,
+    padding: 10,
+    color: 'white',
+  },
+});
